ef193c5b2156e1bf99b71c703f0fd6cc1d10f73a
Davidoumone <david.moneta28@gmail.com>
2022-04-19 04:03:44 +0200

    commenter code

--- js/App.js
+++ js/App.js
@@ -1,18 +1,28 @@
+/* La classe App est la classe principale qui sera utilisée pour créer les cartes vidéo. */
 class App {
-    constructor() {
-        this.$moviesWrapper = document.querySelector('.movies-wrapper')
-        this.moviesApi = new MovieApi('/data/movie-data.json')
-    }
+  /**
+   * La fonction constructeur est une méthode spéciale
+   * pour créer et initialiser un objet créé dans une classe.
+   */
+  constructor() {
+    this.$moviesWrapper = document.querySelector(".movies-wrapper");
+    this.moviesApi = new MovieApi("/data/movie-data.json");
+  }
 
-    async main() {
-        const movies = await this.moviesApi.getMovies()
+  /**
+   * La fonction main() est une fonction asynchrone qui appelle la fonction getMovies() de la classe moviesApi,
+   * puis parcourt le tableau de films et crée un nouvel objet MovieCard pour chaque film du tableau,
+   * puis ajoute la carte de film au DOM.
+   */
+  async main() {
+    const movies = await this.moviesApi.getMovies();
 
-        movies.forEach(movie => {
-            const Template = new MovieCard(movie)
-            this.$moviesWrapper.appendChild(Template.createMovieCard())        
-        })    
-    }
+    movies.forEach((movie) => {
+      const Template = new MovieCard(movie);
+      this.$moviesWrapper.appendChild(Template.createMovieCard());
+    });
+  }
 }
 
-const app = new App()
-app.main()
+const app = new App();
+app.main();
